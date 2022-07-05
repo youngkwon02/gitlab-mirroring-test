e57d9d1f76b8be3e7c9dc7bbc942c0632a9fa548
youngkwon02 <youngkwon02@naver.com>
2022-07-05 23:44:44 +0900

    chore: start Gradle project

---
+++ linklip/src/main/java/com/swmaestro/linklip/LinklipApplication.java
@@ -0,0 +1,13 @@
+package com.swmaestro.linklip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinklipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinklipApplication.class, args);
+	}
+
+}
---
+++ linklip/src/test/java/com/swmaestro/linklip/LinklipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swmaestro.linklip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinklipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
